26b654fe61abb98ba9218a3b51878b92cc31d7d6
leonard.wolf <lionmandota@web.de>
2020-01-25 02:05:18 +0100

    Background Aussehen

--- src/de/lw/remake/Level.java
+++ src/de/lw/remake/Level.java
@@ -1,21 +1,20 @@
 package de.lw.remake;
 
 import de.lw.remake.objects.BaseShip;
+import de.todo.engine.entities.GameObject;
 
-public class Level {
+public class Level extends GameObject {
 
-    Formation[] formations;
+    public Formation[] formations;
 
-    public Level(Formation[] formations) {
-
-        this.formations = formations;
-
+    public Level(float x, float y) {
+        super(x, y);
     }
 
     private class Formation {
         protected BaseShip[] formation;
 
-        protected Formation(BaseShip[] formation){
+        protected Formation(BaseShip[] formation) {
             this.formation = formation;
         }
 
