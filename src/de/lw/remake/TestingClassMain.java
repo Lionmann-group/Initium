8bb8733aec70912f41ef1eb941c639ab82cd712c
Leonard Wolf <leonard.wolf@virtual-solution.com>
2020-02-03 18:03:02 +0100

    Testing stuff

--- src/de/lw/remake/TestingClassMain.java
+++ src/de/lw/remake/TestingClassMain.java
@@ -1,6 +1,5 @@
 package de.lw.remake;
 
-import de.lw.remake.Scene.StartScene;
 import de.todo.engine.GameEngine;
 import de.todo.engine.Window;
 import de.todo.engine.audio.AudioHandler;
@@ -18,6 +17,7 @@ import de.todo.engine.utility.ResourceRepository;
 import org.joml.Vector2f;
 
 import java.awt.Font;
+import java.util.ArrayList;
 
 public class TestingClassMain extends Scene {
 
@@ -27,10 +27,9 @@ public class TestingClassMain extends Scene {
     public static final Vector2f point2 = new Vector2f(750,750);
 
     public int spawnX = 500,spawnY = 500;
-    public int movePhase;
     public TestObject testObject;
 
-    final AudioTrack track = AudioHandler.getInstance().createAudioTrack("/Music/Violet.ogg");
+    private AudioTrack track;
 
     public static void main(final String[] args) {
         ResourceRepository.addRepository(TestingClassMain.class);
@@ -51,11 +50,12 @@ public class TestingClassMain extends Scene {
     @Override
     protected void initScene(Window.WindowDimensions windowDimensions) throws Exception {
         setActiveCamera(new Camera(0, 0));
-
+        track = AudioHandler.getInstance().createAudioTrack("/Music/Violet.ogg");
         AudioHandler.getInstance().playAudioTrack(track, false);
         final GameObject stats = DebugStatistics.getInstance().createTextObject(new Vector2f(0, 0), new Font("Ubuntu Mono", Font.BOLD, 14), GLColor.WHITE, 4);
 
         addGameObjects(
+                stats,
                 testObject = new TestObject(spawnX,spawnY)
         );
 
@@ -65,7 +65,6 @@ public class TestingClassMain extends Scene {
     public void input(IInputState inputState) throws Exception {
         if (inputState.isKeyPressed(KeyState.KEY_SPACE)) {
             testObject.setPosition(new Vector2f(spawnX,spawnY));
-            movePhase = 0;
         }
         if (inputState.isKeyDown(KeyState.KEY_UP)) {
             spawnY--;
@@ -82,20 +81,56 @@ public class TestingClassMain extends Scene {
     }
 
     private class TestObject extends GameObject {
+
+        int projectileTimer;
+        float angle;
+
         public TestObject(float x, float y) {
             super(x, y);
 
             setMesh(new RectangularMesh(100f, 100f));
             setRenderDefinition(new TextureRenderDefinition("/Test/The_Girl_that_gets_my_PP_hard.png"));
             setScale(0.5f);
+            setLayer(0.5f);
         }
 
         @Override
         public void update() {
-
-            //movePhase++;
+            projectileTimer++;
+            if(projectileTimer >= 70){
+                projectileTimer = 0;
+                addChildren(new TestProjectile(getPosition(),angle,this));
+            }
         }
 
+    }
+
+    private class TestProjectile extends GameObject {
+
+        int movePhase;
+        float angle;
+
+        public TestProjectile(Vector2f position, float angle, TestObject parent) {
+            super(position);
+            this.angle = angle;
+
+            setMesh(new RectangularMesh(100f, 100f));
+            setRenderDefinition(new TextureRenderDefinition("/Objects/Energyball.png"));
+            setLayer(1f);
+            setScale(0.1f);
+            useRelativePosition(false);
+
+        }
+
+        @Override
+        public void update() {
+            this.move();
+        }
+
+        public void move() {
+            this.move(new Vector2f(movePhase,-0.2f*angle*movePhase));
+            movePhase++;
+        }
     }
 
 }
