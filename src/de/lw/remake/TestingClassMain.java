0c60b18f6c4a9df40cdd50010443a643ffe2c71f
Felix Vogel <20129564+FelixVogel@users.noreply.github.com>
2020-02-04 09:22:47 +0100

    Some small changes

--- src/de/lw/remake/TestingClassMain.java
+++ src/de/lw/remake/TestingClassMain.java
@@ -1,6 +1,5 @@
 package de.lw.remake;
 
-import com.sun.jna.platform.unix.X11;
 import de.todo.engine.GameEngine;
 import de.todo.engine.Window;
 import de.todo.engine.audio.AudioHandler;
@@ -15,19 +14,19 @@ import de.todo.engine.render.mesh.RectangularMesh;
 import de.todo.engine.scene.Scene;
 import de.todo.engine.utility.DebugStatistics;
 import de.todo.engine.utility.ResourceRepository;
+import de.todo.engine.utility.Timer;
 import org.joml.Vector2f;
 
 import java.awt.Font;
-import java.util.ArrayList;
 
 public class TestingClassMain extends Scene {
 
     public static final int WINDOW_WIDTH = 1000;
     public static final int WINDOW_HEIGHT = 1000;
-    public static final Vector2f point1 = new Vector2f(250,250);
-    public static final Vector2f point2 = new Vector2f(750,750);
+    public static final Vector2f point1 = new Vector2f(250, 250);
+    public static final Vector2f point2 = new Vector2f(750, 750);
 
-    public int spawnX = 500,spawnY = 500;
+    public int spawnX = 500, spawnY = 500;
     public TestObject testObject;
 
     private AudioTrack track;
@@ -51,13 +50,16 @@ public class TestingClassMain extends Scene {
     @Override
     protected void initScene(Window.WindowDimensions windowDimensions) throws Exception {
         setActiveCamera(new Camera(0, 0));
+
         track = AudioHandler.getInstance().createAudioTrack("/Music/Violet.ogg");
+
         AudioHandler.getInstance().playAudioTrack(track, false);
+
         final GameObject stats = DebugStatistics.getInstance().createTextObject(new Vector2f(0, 0), new Font("Ubuntu Mono", Font.BOLD, 14), GLColor.WHITE, 4);
 
         addGameObjects(
                 stats,
-                testObject = new TestObject(spawnX,spawnY)
+                testObject = new TestObject(spawnX, spawnY)
         );
 
     }
@@ -65,17 +67,21 @@ public class TestingClassMain extends Scene {
     @Override
     public void input(IInputState inputState) throws Exception {
         if (inputState.isKeyPressed(KeyState.KEY_SPACE)) {
-            testObject.setPosition(new Vector2f(spawnX,spawnY));
+            testObject.setPosition(new Vector2f(spawnX, spawnY));
         }
+
         if (inputState.isKeyDown(KeyState.KEY_UP)) {
             spawnY--;
         }
+
         if (inputState.isKeyDown(KeyState.KEY_DOWN)) {
             spawnY++;
         }
+
         if (inputState.isKeyDown(KeyState.KEY_RIGHT)) {
             spawnX++;
         }
+
         if (inputState.isKeyDown(KeyState.KEY_LEFT)) {
             spawnX--;
         }
@@ -97,11 +103,13 @@ public class TestingClassMain extends Scene {
 
         @Override
         public void update() {
-            angle = angle + 0.1f;
+            angle += 1.0f * Timer.getTimeDelta();
             projectileTimer++;
-            if(projectileTimer >= 20){
+
+            if (projectileTimer >= 20) {
                 projectileTimer = 0;
-                addChildren(new TestProjectile(new Vector2f(getPosition()),angle,this));
+
+                addChildren(new TestProjectile(new Vector2f(getPosition()), angle, this));
             }
         }
 
@@ -116,9 +124,9 @@ public class TestingClassMain extends Scene {
             super(position);
             this.angle = angle;
 
-            setMesh(new RectangularMesh(100f, 100f));
+            setMesh(new RectangularMesh(100.0f, 100.0f));
             setRenderDefinition(new TextureRenderDefinition("/Objects/Energyball.png"));
-            setLayer(1f);
+            setLayer(1.0f);
             setScale(0.3f);
             useRelativePosition(false);
 
@@ -126,13 +134,13 @@ public class TestingClassMain extends Scene {
 
         @Override
         public void update() {
-            this.move();
+            move();
         }
 
         public void move() {
-                movePhase = movePhase + 10;
-            this.move(new Vector2f(movePhase,-0.01f*angle*movePhase));
+            movePhase = movePhase + 10;
 
+            move(new Vector2f(movePhase, -0.01f * angle * movePhase));
         }
     }
 
